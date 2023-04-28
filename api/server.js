27a306d917f4c6f25bd9bab27a44adf6193af97d
rewanhamed22 <rewanhamed3@gmail.com>
2023-04-28 17:09:15 +0300

    changes

---
+++ api/server.js
@@ -0,0 +1,28 @@
+// See https://github.com/typicode/json-server#module
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+server.db = router.db;
+// Add this before server.use(router)
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
+
+server.use(auth);
+
+server.use(router);
+
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
